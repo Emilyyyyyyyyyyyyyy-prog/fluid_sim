e236b4ad4cc017f68a0dd439846f400aaef6ee86
Emily <58360984+Emilyyyyyyyyyyyyyy-prog@users.noreply.github.com>
2024-04-21 21:28:49 +0300

    change vector to dynamic array

--- Field.cpp
+++ Field.cpp
@@ -1,21 +1,15 @@
 #include "Field.h"
-#include <vector>
 
-Field::Field() {
-	this->x = 200;
-	this->y = 200;
-	this->xy = (this->x + 2) * (this->y + 2);
-	for (int i = 0; i < this->xy; i++) {
-		this->values.push_back(0);
-	}
-}
+
+Field::Field() {}
 
 void Field::set(int xSize, int ySize) {
 	this->x = xSize;
 	this->y = ySize;
 	this->xy = (this->x + 2) * (this->y + 2);
+	this->values = new float[(xSize + 2) * (ySize + 2)];
 	for (int i = 0; i < this->xy; i++) {
-		this->values.push_back(0);
+		this->values[i] = 0;
 	}
 }
 
