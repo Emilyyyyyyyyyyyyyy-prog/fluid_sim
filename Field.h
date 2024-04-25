614d7fbd9284539628fc14dd339c72c4ecdab328
Emily <58360984+Emilyyyyyyyyyyyyyy-prog@users.noreply.github.com>
2024-04-25 23:56:55 +0300

    add comments

--- Field.h
+++ Field.h
@@ -1,10 +1,10 @@
 class Field {
 private:
-	int x, y, xy; //field dimentions
-	float* values; //containing the field parameters
+	int x, y, xy;  //field dimensions
+	float* values;  //array containing the field parameters
 public:
 	Field();
-	void set(int xSize, int ySize);
+	void set(int xSize, int ySize);  //set size and values
 	~Field();
 
 	friend class Fluid;
