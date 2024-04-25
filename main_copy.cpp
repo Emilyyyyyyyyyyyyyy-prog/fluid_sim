ae476183456899190bbf5e458a715363b076fd4c
Lokhmatov Arseniy <lokhmatov.ai@phystech.edu>
2024-04-26 01:41:00 +0300

    revision of the design

--- main_copy.cpp
+++ main_copy.cpp
@@ -1,9 +1,8 @@
-#include "Fluid.h"
-//#include "Window.cpp"
-//#include "roundedRect.cpp"
 #include <SFML/Graphics.hpp>
-#include "SFC/Svg.hpp"
+#include <iostream>
 #include <windows.h>
+#include <string>
+#include "Fluid.h"
 
 int main() {
 	sf::Font font;
@@ -11,6 +10,9 @@ int main() {
 		std::cerr << "Failed to load font!" << std::endl;
 		return -1;
 	}
+
+	double diffusion = 0.07;
+	double viscosity = 0.997;
 
 	//fluid parameters
 	int size_x = 400;
@@ -28,7 +30,7 @@ int main() {
 	window.setFramerateLimit(framerate);
 
 	unsigned int flag = 0, mig = 0;
-	bool flagMig = true, flagMenu = true, flagClose = false;;
+	bool flagMig = true, flagMenu = false, flagClose = false, flagJet = false, flagGo = false;
 
 	//flag == 0
 	sf::Text labelMain;
@@ -84,6 +86,19 @@ int main() {
 	labelOut.setFillColor(sf::Color::White);
 	labelOut.setPosition(x - 35, y - 30);
 
+	sf::RectangleShape rectHome(sf::Vector2f(300, 40));
+	rectHome.setOrigin(150, 20);
+	rectHome.setFillColor(sf::Color::Black);
+	rectHome.setPosition(160, window.getSize().y - 50);
+	rectHome.setOutlineThickness(2);
+	rectHome.setOutlineColor(sf::Color(102, 102, 102));
+	sf::Text labelHome;
+	labelHome.setFont(font);
+	labelHome.setString("Home");
+	labelHome.setCharacterSize(40);
+	labelHome.setFillColor(sf::Color::White);
+	labelHome.setPosition(120, window.getSize().y - 80);
+
 	sf::Text labelVers;
 	labelVers.setFont(font);
 	labelVers.setString("Version 1.0.1");
@@ -113,19 +128,119 @@ int main() {
 	rectM3.setPosition(14, 58);
 
 	//flagClose == true
-	sf::RectangleShape rectClose(sf::Vector2f(400, 150));
+	sf::RectangleShape rectClose(sf::Vector2f(600, 150));
 	rectClose.setFillColor(sf::Color::Black);
-	rectClose.setOrigin(200, 125);
+	rectClose.setOrigin(300, 125);
 	rectClose.setPosition(window.getSize().x / 2, window.getSize().y / 2);
 	rectClose.setOutlineThickness(2);
-	rectClose.setOutlineColor(sf::Color(102, 102, 102));
+	rectClose.setOutlineColor(sf::Color::Red);
 
-	sf::Text labelSet;
-	labelSet.setFont(font);
-	labelSet.setString("Really close the window?");
-	labelSet.setCharacterSize(40);
-	labelSet.setFillColor(sf::Color::White);
-	labelSet.setPosition(window.getSize().x / 2 - , y - 30);
+	sf::Text labelClose;
+	labelClose.setFont(font);
+	labelClose.setString("Really close the window?");
+	labelClose.setCharacterSize(40);
+	labelClose.setFillColor(sf::Color::White);
+	labelClose.setPosition(rectClose.getPosition().x - 270, rectClose.getPosition().y - 120);
+
+	sf::RectangleShape rectYes(sf::Vector2f(100, 50));
+	rectYes.setFillColor(sf::Color::Black);
+	rectYes.setOrigin(50, 25);
+	rectYes.setPosition(window.getSize().x / 2 - 100, window.getSize().y / 2 - 25);
+	rectYes.setOutlineThickness(2);
+	rectYes.setOutlineColor(sf::Color(102, 102, 102));
+
+	sf::Text labelYes;
+	labelYes.setFont(font);
+	labelYes.setString("Yes");
+	labelYes.setCharacterSize(40);
+	labelYes.setFillColor(sf::Color::White);
+	labelYes.setPosition(rectYes.getPosition().x - 35, rectYes.getPosition().y - 30);
+
+	sf::RectangleShape rectNo(sf::Vector2f(100, 50));
+	rectNo.setFillColor(sf::Color::Black);
+	rectNo.setOrigin(50, 25);
+	rectNo.setPosition(window.getSize().x / 2 + 100, window.getSize().y / 2 - 25);
+	rectNo.setOutlineThickness(2);
+	rectNo.setOutlineColor(sf::Color(102, 102, 102));
+
+	sf::Text labelNo;
+	labelNo.setFont(font);
+	labelNo.setString("No");
+	labelNo.setCharacterSize(40);
+	labelNo.setFillColor(sf::Color::White);
+	labelNo.setPosition(rectNo.getPosition().x - 20, rectNo.getPosition().y - 30);
+
+	//flag == 2
+	//поле ввода
+	sf::RectangleShape rectInput1(sf::Vector2f(300, 40));
+	rectInput1.setOrigin(rectInput1.getSize().x / 2, rectInput1.getSize().y / 2);
+	rectInput1.setPosition(window.getSize().x / 2 + 160, window.getSize().y / 2 - 50);
+	rectInput1.setFillColor(sf::Color::Black);
+	rectInput1.setOutlineThickness(2);
+	rectInput1.setOutlineColor(sf::Color(102, 102, 102));
+	//ввод текста
+	sf::Text inputText1;
+	inputText1.setFont(font);
+	inputText1.setString("0.07");
+	inputText1.setCharacterSize(40);
+	inputText1.setFillColor(sf::Color::White);
+	inputText1.setPosition(rectInput1.getPosition().x - 140, rectInput1.getPosition().y - 30);
+	bool textEntered1 = false;
+	int count1 = 4;
+	//имя ячейки
+	sf::Text labelInput1;
+	labelInput1.setFont(font);
+	labelInput1.setString("Diffusion coefficient");
+	labelInput1.setCharacterSize(40);
+	labelInput1.setFillColor(sf::Color::White);
+	labelInput1.setPosition(rectInput1.getPosition().x - 600, rectInput1.getPosition().y - 30);
+	bool activ1 = false;
+
+	//поле ввода
+	sf::RectangleShape rectInput2(sf::Vector2f(300, 40));
+	rectInput2.setOrigin(rectInput1.getSize().x / 2, rectInput1.getSize().y / 2);
+	rectInput2.setPosition(window.getSize().x / 2 + 160, window.getSize().y / 2);
+	rectInput2.setFillColor(sf::Color::Black);
+	rectInput2.setOutlineThickness(2);
+	rectInput2.setOutlineColor(sf::Color(102, 102, 102));
+	//ввод текста
+	sf::Text inputText2;
+	inputText2.setFont(font);
+	inputText2.setString("0.997");
+	inputText2.setCharacterSize(40);
+	inputText2.setFillColor(sf::Color::White);
+	inputText2.setPosition(rectInput2.getPosition().x - 140, rectInput2.getPosition().y - 30);
+	bool textEntered2 = false;
+	int count2 = 5;
+	//имя ячейки
+	sf::Text labelInput2;
+	labelInput2.setFont(font);
+	labelInput2.setString("Viscosity coefficient");
+	labelInput2.setCharacterSize(40);
+	labelInput2.setFillColor(sf::Color::White);
+	labelInput2.setPosition(rectInput2.getPosition().x - 600, rectInput2.getPosition().y - 30);
+	bool activ2 = false;
+
+	sf::RectangleShape rectSave(sf::Vector2f(300, 40));
+	rectSave.setOrigin(rectSave.getSize().x / 2, rectInput1.getSize().y / 2);
+	rectSave.setPosition(window.getSize().x / 2 + 160, window.getSize().y / 2 + 50);
+	rectSave.setFillColor(sf::Color::Black);
+	rectSave.setOutlineThickness(2);
+	rectSave.setOutlineColor(sf::Color(102, 102, 102));
+	sf::Text labelSave;
+	labelSave.setFont(font);
+	labelSave.setString("Save");
+	labelSave.setCharacterSize(40);
+	labelSave.setFillColor(sf::Color::White);
+	labelSave.setPosition(rectSave.getPosition().x - 50, rectSave.getPosition().y - 30);
+	bool activ3 = false;
+
+	sf::Text labelSettings;
+	labelSettings.setFont(font);
+	labelSettings.setString("Settings");
+	labelSettings.setCharacterSize(80);
+	labelSettings.setFillColor(sf::Color::White);
+	labelSettings.setPosition(100, -15);
 
 	while (window.isOpen())
 	{
@@ -137,41 +252,156 @@ int main() {
 				window.close();
 			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
 				window.close();
-		}
-
-		if (event.type == sf::Event::MouseButtonPressed) {
 			if (event.mouseButton.button == sf::Mouse::Left &&
-				rectRun.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
-				flag = 1;
-				std::cout << 1 << std::endl;
+				rectInput1.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+				activ1 = true;
+				rectInput1.setOutlineColor(sf::Color::Yellow);
+				rectInput2.setOutlineColor(sf::Color(102, 102, 102));
+				rectSave.setOutlineColor(sf::Color(102, 102, 102));
+				activ2 = false, activ3 = false;
 			}
-			if (event.mouseButton.button == sf::Mouse::Left &&
-				rectSet.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
-				flag = 2;
-				std::cout << 2 << std::endl;
-			}
-			if (event.mouseButton.button == sf::Mouse::Left &&
-				rectOut.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
-				flagClose = true;
-			}
-			if (flagMenu)
-			{
-				if (sf::Mouse::getPosition().x < rectLine.getPosition().x) {
-					rectLine.setPosition(rectLine.getPosition().x + 250, rectLine.getPosition().y);
-					flagMenu = false;
-					rectSet.setPosition(160, 110);
-					labelSet.setPosition(75, 80);
-					rectOut.setPosition(160, 170);
-					labelOut.setPosition(125, 140);
+			if (event.type == sf::Event::TextEntered && activ1) {
+				if (event.text.unicode < 128) {
+					if (event.text.unicode == 8 && !inputText1.getString().isEmpty()) {
+						std::string inputString = inputText1.getString();
+						inputString.erase(inputString.size() - 1, 1);
+						inputText1.setString(inputString);
+						count1--;
+					}
+					else if (event.text.unicode == 13) {
+						std::string inputString = inputText1.getString();
+						activ1 = false;
+						rectInput1.setOutlineColor(sf::Color(102, 102, 102));
+					}
+					else if (event.text.unicode >= 48 && event.text.unicode <= 57 || static_cast<char>(event.text.unicode) == '.') {
+						std::string inputString = inputText1.getString();
+						if (count1 <= 7) {
+							inputString += static_cast<char>(event.text.unicode);
+							count1++;
+							inputText1.setString(inputString);
+						}
+					}
 				}
 			}
-			else
-			{
+			if (event.mouseButton.button == sf::Mouse::Left &&
+				rectInput2.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+				activ2 = true;
+				rectInput2.setOutlineColor(sf::Color::Yellow);
+				rectInput1.setOutlineColor(sf::Color(102, 102, 102));
+				rectSave.setOutlineColor(sf::Color(102, 102, 102));
+				activ1 = false, activ3 = false;
+			}
+			if (event.type == sf::Event::TextEntered && activ2) {
+				if (event.text.unicode < 128) {
+					if (event.text.unicode == 8 && !inputText2.getString().isEmpty()) {
+						std::string inputString = inputText2.getString();
+						inputString.erase(inputString.size() - 1, 1);
+						inputText2.setString(inputString);
+						count2--;
+					}
+					else if (event.text.unicode == 13) {
+						std::string inputString = inputText2.getString();
+						activ2 = false;
+						rectInput2.setOutlineColor(sf::Color(102, 102, 102));
+					}
+					else if (event.text.unicode >= 48 && event.text.unicode <= 57 || static_cast<char>(event.text.unicode) == '.') {
+						std::string inputString = inputText2.getString();
+						if (count2 <= 7) {
+							inputString += static_cast<char>(event.text.unicode);
+							count2++;
+							inputText2.setString(inputString);
+						}
+					}
+				}
+			}
+			if (event.type == sf::Event::MouseButtonPressed) {
+				if (event.mouseButton.button == sf::Mouse::Left && !flagClose &&
+					rectRun.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+					flag = 1;
+					flagGo = true;
+					if (flagMenu) {
+						rectLine.setPosition(rectLine.getPosition().x - 250, rectLine.getPosition().y);
+						flagMenu = false;
+					}
+					std::cout << 1 << std::endl;
+				}
+				if (event.mouseButton.button == sf::Mouse::Left && !flagClose &&
+					rectSet.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+					flag = 2;
+					flagGo = true;
+					if (flagMenu) {
+						rectLine.setPosition(rectLine.getPosition().x - 250, rectLine.getPosition().y);
+						flagMenu = false;
+					}
+					std::cout << 2 << std::endl;
+				}
+				if (event.mouseButton.button == sf::Mouse::Left && !flagClose &&
+					rectOut.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+					flagClose = true;
+				}
+				if (event.mouseButton.button == sf::Mouse::Left && !flagClose &&
+					rectHome.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+					flag = 0;
+					flagGo = false;
+					flagMenu = false;
+					x = window.getSize().x / 2;
+					y = window.getSize().y / 2 - 100;
+					rectRun.setPosition(x, y);
+					labelRun.setPosition(x - 55, y - 30);
+
+					y += 75;
+
+					rectSet.setPosition(x, y);
+					labelSet.setPosition(x - 85, y - 30);
+
+					y += 75;
+
+					rectOut.setPosition(x, y);
+					labelOut.setPosition(x - 35, y - 30);
+					rectLine.setPosition(rectLine.getPosition().x - 250, rectLine.getPosition().y);
+					std::cout << 1 << std::endl;
+				}
 				if (event.mouseButton.button == sf::Mouse::Left && !flagMenu &&
 					rectMenu.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
-					rectLine.setPosition(rectLine.getPosition().x - 250, rectLine.getPosition().y);
+					rectLine.setPosition(rectLine.getPosition().x + 250, rectLine.getPosition().y);
 					flagMenu = true;
 				}
+				else if (event.mouseButton.button == sf::Mouse::Left && flagMenu &&
+					rectMenu.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+					rectLine.setPosition(rectLine.getPosition().x - 250, rectLine.getPosition().y);
+					flagMenu = false;
+				}
+				if (event.mouseButton.button == sf::Mouse::Left &&
+					rectNo.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+					flagClose = false;
+				}
+				if (event.mouseButton.button == sf::Mouse::Left &&
+					rectYes.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+					window.close();
+				}
+				if (flagGo) {
+					rectRun.setPosition(160, 110);
+					labelRun.setPosition(100, 80);
+					rectSet.setPosition(160, 170);
+					labelSet.setPosition(75, 140);
+					rectOut.setPosition(160, 230);
+					labelOut.setPosition(125, 200);
+				}
+			}
+			if (event.mouseButton.button == sf::Mouse::Left &&
+				rectSave.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+				rectInput1.setOutlineColor(sf::Color(102, 102, 102));
+				rectInput2.setOutlineColor(sf::Color(102, 102, 102));
+				std::string tmp1 = inputText1.getString();
+				std::string tmp2 = inputText2.getString();
+				if (std::stod(tmp1) > 0 && std::stod(tmp1) <= 1 && std::stod(tmp2) > 0 && std::stod(tmp2) <= 1) {
+					diffusion = std::stod(tmp1), viscosity = std::stod(tmp2);
+					rectSave.setOutlineColor(sf::Color::Green);
+				}
+				else {
+					rectSave.setOutlineColor(sf::Color::Red);
+				}
+
 			}
 		}
 
@@ -186,24 +416,47 @@ int main() {
 			window.draw(labelOut);
 			window.draw(labelVers);
 		}
-		else if (flag == 1) {
+		else if (flag == 1 || flag == 2) {
 			window.draw(rectLine);
 			window.draw(rectMenu);
 			window.draw(rectM2);
-			if (flagMenu) {
+			if (!flagMenu) {
 				window.draw(rectM1);
 				window.draw(rectM3);
 			}
 			else {
-				window.draw(rectSet);
-				window.draw(labelSet);
+				if (flagGo) {
+					window.draw(rectSet);
+					window.draw(labelSet);
+					window.draw(rectRun);
+					window.draw(labelRun);
+					window.draw(rectHome);
+					window.draw(labelHome);
+				}
 				window.draw(rectOut);
 				window.draw(labelOut);
 			}
 		}
-
+		if (flag == 2) {
+			if (flagMenu && labelSettings.getPosition().x == 100) labelSettings.setPosition(labelSettings.getPosition().x + 250, labelSettings.getPosition().y);
+			if (!flagMenu && labelSettings.getPosition().x == 350) labelSettings.setPosition(labelSettings.getPosition().x - 250, labelSettings.getPosition().y);
+			window.draw(labelSettings);
+			window.draw(rectInput1);
+			window.draw(labelInput1);
+			window.draw(inputText1);
+			window.draw(rectInput2);
+			window.draw(labelInput2);
+			window.draw(inputText2);
+			window.draw(rectSave);
+			window.draw(labelSave);
+		}
 		if (flagClose) {
 			window.draw(rectClose);
+			window.draw(rectYes);
+			window.draw(rectNo);
+			window.draw(labelYes);
+			window.draw(labelNo);
+			window.draw(labelClose);
 		}
 		Sleep(100);
 		if (mig < 5 and flagMig) {
